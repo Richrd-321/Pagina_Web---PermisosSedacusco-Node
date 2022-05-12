a08b4083a0477603f59b20976a91f9d70365a696
Diego Ortiz Carmelino <dortiz@SEDACUSCO.COM>
2022-05-12 12:29:55 -0500

    1er trabajo de boletas desde ña Oficina 12/05/2022

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require('sequelize');
+
+
+// Option 3: Passing parameters separately (other dialects)
+const db = new Sequelize('upTasknode', 'root', 'root', {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: '3307', // DB en la Oficina
+    //port: '3306', // DB en la Casa
+    operatorsAliases: false,
+    define: {
+        timestamps: false
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const routes = require('./routes');
+/*
+const path = require('path');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('./config/passport');
+
+
+
+// Helpers con algunas funciones
+const helpers = require('./helpers');
+
+// Crear la conexion a la base de datso
+const db = require('./config/db');
+// const cookieParser = require('cookie-parser');
+
+// Importar el modelo
+require('./models/Proyectos');
+require('./models/Tareas');
+require('./models/Usuarios');
+
+//db.authenticate()
+db.sync()
+    .then(() => console.log('Conectado al Servidor'))
+    .catch(error => console.log(error));
+*/
+// Crear una app de express
+const app = express();
+
+/*
+// Donde cargar los archivos estaticos
+app.use(express.static('public'));
+
+// Habilitar Pug
+app.set('view engine', 'pug');
+
+// Habilitar bodyParser para leer datos del formulario
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Agregamos express validator a toda la aplicación
+app.use(expressValidator());
+
+// Añadir la carpeta de las vistas
+app.set('views', path.join(__dirname, './views'));
+
+// Agregar flash messages
+app.use(flash());
+
+app.use(cookieParser());
+
+// Sesiones nos permite navegar entre distintas paginas vin vovernos a utenticar
+app.use(session({
+    secret: 'supersecreto',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Use el Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Pasar var dump a la aplicacion
+app.use((req, res, next) => {
+    res.locals.vardump = helpers.vardump;
+    res.locals.mensajes = req.flash();
+    res.locals.usuario = {...req.user} || null;
+    next();
+});
+
+// Ruta para el home
+app.use('/', routes());
+*/
+
+app.use('/',routes());
+
+app.listen(3000);
